ba1b7d6d9c897e2bc2425c33c0bc2f144d6dc358
MikiUA <106928450+MikiUA@users.noreply.github.com>
2023-06-04 18:13:08 +0300

    added ErrorHandler for controllers and customError for errorResponces

---
+++ helpers/errorHandler.js
@@ -0,0 +1,28 @@
+const wrapper = fn => async (req, res, next) => {
+    try {
+        const result = await fn(req, res, next);
+        return result;
+    } catch (error) {
+        switch (error.name) {
+            case 'ValidationError':
+                res
+                    .status(400)
+                    .send({ status: 'error', code: 400, message: error.message });
+                break;
+            case 'CustomError':
+                res.status(error.status).send({
+                    status: 'error',
+                    code: error.status,
+                    message: error.message,
+                });
+                break;
+            default:
+                res.status(500).send({
+                    status: 'error',
+                    message: "Internal Server Error. Sorry for inconvenience",
+                });
+        }
+    }
+};
+
+module.exports = wrapper;
--- routes/authRouter/index.js
+++ routes/authRouter/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { login, register, getNewToken, logout } = require('../../controllers/auth/controllers');
+const handled = require('../../helpers/errorHandler')
 
-
-router.post('/login', login);
-router.post('/signup', register);
-router.post('/register', register);
-router.get('/refreshToken', getNewToken);
-router.delete('/logout', logout);
+router.post('/login', handled(login));
+router.post('/signup', handled(register));
+router.post('/register', handled(register));
+router.get('/refreshToken', handled(getNewToken));
+router.delete('/logout', handled(logout));
 
 module.exports = { router }
