b7f4760239c7df21e86299fa8d271d0245cf4119
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2023-06-07 20:45:48 +0300

    auth for reviews

--- routes/reviewsRouter/index.js
+++ routes/reviewsRouter/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { EMPTYHANDLER } = require("../TODOhandler");
-const reviewCntrl = require("../../controllers/index");
 
-router.get("/reviews", reviewCntrl.getAllReviews);
-router.get("/my-reviews", reviewCntrl.getUserReviews);
-router.post("/my-reviews", reviewCntrl.addReview);
-router.patch("/:reviewID", reviewCntrl.editReview);
-router.delete("/:reviewID", reviewCntrl.removeReview);
+const reviewCntrl = require("../../controllers/index");
+const { authentificateUser } = require("../../middleware");
+
+router.get("/reviews", authentificateUser, reviewCntrl.getAllReviews);
+router.get("/my-reviews", authentificateUser, reviewCntrl.getUserReviews);
+router.post("/my-reviews", authentificateUser, reviewCntrl.addReview);
+router.patch("/:reviewID", authentificateUser, reviewCntrl.editReview);
+router.delete("/:reviewID", authentificateUser, reviewCntrl.removeReview);
 
 module.exports = { router };
